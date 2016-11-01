5e3be6f8b7d79e626aef933cf742431e92e1b96c
Iwona <iwonajankowsky@gmail.com>
2016-11-01 21:43:38 +0100

    paper.js library

--- src/app/components/initializator.component.ts
+++ src/app/components/initializator.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 import {Game} from '../models/Game';
 import {Player} from '../models/Player';
+declare var paper:any; 
 
 
 @Component({
@@ -11,17 +12,57 @@ import {Player} from '../models/Player';
 
 
 export class InitializatorComponent {
-	constructor() { }
+	constructor() { 
+		
+	}
 
     helloWorld():void {
-      console.log('Hello world1');
 
-      let game : Game = new Game(1, 1, null, 1, [new Player(1, 1, 1, 1, 1, '')]);
+        console.log('Hello world');
 
-      console.log(game);
-      $.get("/assets/php/GetGame.php?status=STARTED&gameId=21", function(data, status){
-        console.log("Data: " + data.toString() + "\nStatus: " + status);
-      });
+        let game : Game = new Game(1, 1, null, 1, [new Player(1, 1, 1, 1, 1, '')]);
+
+        console.log(game); 
+
+        this.draw();
+    }
+
+    draw():void {
+    	var canvas = document.getElementById("myCanvas");
+	    paper.setup(canvas);
+		var points = 25;
+		var length = 35;
+		var path = new paper.Path({
+			strokeColor: '#E4141B',
+			strokeWidth: 20,
+			strokeCap: 'round'
+		});
+
+		var start = paper.view.center.divide([10, 1]);
+		for (var i = 0; i < points; i++)
+			path.add(start.add(new paper.Point(i * length, 0)));
+
+		paper.view.onMouseMove = function(event)  {
+			path.firstSegment.point = event.point;
+			for (var i = 0; i < points - 1; i++) {
+				var segment = path.segments[i];
+				var nextSegment = segment.next;
+				var vector = segment.point.subtract(nextSegment.point);
+				vector.length = length;
+				nextSegment.point = segment.point.subtract(vector);
+			}
+			path.smooth({ type: 'continuous' });
+		};
+
+		paper.view.onMouseDown = function(event) {
+			path.fullySelected = true;
+			path.strokeColor = '#e08285';
+		}
+
+		paper.view.onMouseUp = function(event) {
+			path.fullySelected = false;
+			path.strokeColor = '#e4141b';
+		}
     }
 
 }
