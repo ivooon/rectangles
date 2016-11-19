0fb2f3edbd81f50ce63bba2d2e39e886a610f457
Iwona <iwonajankowsky@gmail.com>
2016-11-19 20:16:18 +0100

    drawing of rectangles

--- src/app/components/initializator.component.ts
+++ src/app/components/initializator.component.ts
@@ -60,45 +60,36 @@ export class InitializatorComponent {
 	}
 
     startGame():void {
-      this._InteractionFacadeImpl.startGame();
+     	this._InteractionFacadeImpl.startGame();
         this.draw();
     }
 
     draw():void {
+
     	var canvas = document.getElementById("myCanvas");
 	    paper.setup(canvas);
-		var points = 25;
-		var length = 35;
-		var path = new paper.Path({
-			strokeColor: '#E4141B',
-			strokeWidth: 20,
-			strokeCap: 'round'
-		});
+	    paper.view.remove();
+	 	var tool = new paper.Tool();
+	 	var firstPoint, block, w, h;
 
-		var start = paper.view.center.divide([10, 1]);
-		for (var i = 0; i < points; i++)
-			path.add(start.add(new paper.Point(i * length, 0)));
+	    paper.view.onMouseDown = function(event) {
+	    	firstPoint = event.point;
+	    	console.log(firstPoint)
+	    	block = new paper.Path.Rectangle(firstPoint, {width: 1, height: 1});
+	    	block.fillColor = '#000';
+		}
+		
+		tool.onMouseDrag = function(event) {
+			h = event.point.y - firstPoint.y;
+			w = event.point.x - firstPoint.x;
 
-		paper.view.onMouseMove = function(event)  {
-			path.firstSegment.point = event.point;
-			for (var i = 0; i < points - 1; i++) {
-				var segment = path.segments[i];
-				var nextSegment = segment.next;
-				var vector = segment.point.subtract(nextSegment.point);
-				vector.length = length;
-				nextSegment.point = segment.point.subtract(vector);
-			}
-			path.smooth({ type: 'continuous' });
-		};
-
-		paper.view.onMouseDown = function(event) {
-			path.fullySelected = true;
-			path.strokeColor = '#e08285';
+			block.remove();
+			block = new paper.Path.Rectangle(firstPoint, {width: w, height: h});
+			block.fillColor = '#000';
 		}
 
 		paper.view.onMouseUp = function(event) {
-			path.fullySelected = false;
-			path.strokeColor = '#e4141b';
+			// TODO save block
 		}
     }
 
